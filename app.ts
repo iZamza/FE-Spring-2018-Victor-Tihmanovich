a4e3bfc43c121dfc2231c9b6403417e8d833241b
Victor Tihmanovich <iZamza@gmail.com>
2018-09-19 21:54:55 +0300

    working app

--- app.ts
+++ app.ts
@@ -1,70 +1,89 @@
-interface User{
-	id : number,
-	name : string,
-	password: string,
-	dateOfBirth: string,
-	dateOfLirstLogin: string,
-	dateOfNextNotification: string,
-	information: string
-}
-import * as express  from 'express';
+import * as express from 'express';
+import { Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
 
-let users = require('./users.json');
-const app = express();
+
+interface User {
+  id: number,
+  name: string,
+  password: string,
+  dateOfBirth: string,
+  dateOfFirstLogin: string,
+  dateOfNextNotification: string,
+  information: string
+}
+
+const app: express.Application = express();
+const port: number = 8888;
 
 app.use(bodyParser.json());
-
-app.use((_, res, next) => {
-    res.header('Content-Type', 'application/json');
-    next();
+app.use((_, response, next) => {
+  response.header('Content-Type', 'application/json');
+  next();
 });
 
-app.get('/users', (_, res) => {
-    res.send(users);
+let users: User[] = require('./users.json')
+
+const findUser = (request: Request) => {
+  return users.find((user: User) => user.id == request.params.id);
+};
+
+const userNotFoundError = (response: Response) => {
+  response.statusCode = 404;
+
+  response.send('There is no users with such id!');
+}
+
+app.get('/users', (_, response) => {
+  response.send(users);
 });
 
-app.get('/users/:id', (req, res) => {
-    let result = users.find(user => user.id == req.params.id);
-    if (!result) {
-    	res.status = 404;
-        res.send('There is no user with such id');
-    }
-    else {
-        res.send(result);
-    }
+app.get('/users/:id', (request, response) => {
+  const user = findUser(request);
+
+  if (!user) {
+    userNotFoundError(response);
+  }
+  else {
+    response.send(user);
+  }
 });
 
-app.post('/users/add', (req, res) => {
-    let newUser = req.body;
-    users.push(newUser);
-    res.send(newUser);
+app.post('/users/add', (request, response) => {
+  const newUser = request.body;
+  console.log(newUser);
+  users.push(newUser);
+
+  response.send(newUser);
 });
 
-app.put('/users/:id', (req, res) => {
-    let currentUser = users.find(user => user.id == req.params.id);
-    if (!currentUser) {
-    	res.statusCode = 404;
-        res.send('There is no user with such id');
-    }
-    else {
-       let userIndex = users.indexOf(currentUser);
-       users[userIndex] = req.body;
-       res.send(users[userIndex]);
-    }
+app.put('/users/:id', (request, response) => {
+  const user = findUser(request);
+
+  if (!user) {
+    userNotFoundError(response);
+  }
+  else {
+    const userIndex = users.findIndex((user: User) => user.id == request.params.id);
+    users[userIndex] = request.body;
+
+    response.send(users[userIndex]);
+  }
 });
 
-app.delete('/users/:id', (req, res) => {
-    let currentUser = users.find(user => user.id == req.params.id);
-    if (!currentUser) {
-    	res.status = 404;
-        res.send('There is no user with such id');
-    }
-    else {
-       let userIndex = users.indexOf(currentUser);
-       users = users.filter(user => user !== users[userIndex]);
-       res.send(users);
-    }
+app.delete('/users/:id', (request, response) => {
+  const user = findUser(request);
+
+  if (!user) {
+    userNotFoundError(response);
+  }
+  else {
+    users = users.filter((user: User) => user.id !== request.params.id);
+    
+    response.send(users);
+  }
 });
 
-app.listen(8888);
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});

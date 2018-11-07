5dcf6d0608ac88ea0aff8fa000e4c5d954676b83
Victor Tihmanovich <iZamza@gmail.com>
2018-11-07 13:37:31 +0300

    Add filter in tree

--- src/app/tree-list/tree-list/tree-list.component.ts
+++ src/app/tree-list/tree-list/tree-list.component.ts
@@ -9,7 +9,7 @@ export class TreeListComponent implements OnInit {
   itemList;
   deep = 0;
   itemPath = [];
-  constructor() { }
+  constructor() {}
   ngOnInit() {
     this.createList();
   }
@@ -20,39 +20,54 @@ export class TreeListComponent implements OnInit {
   }
 
   getDeep() {
-   return this.deep;
+    return this.deep;
   }
 
   chooseItem(event) {
     const currentItem: HTMLElement = event.target;
     const itemClassName = event.target.className;
-    const itemDeep = event.target.dataset.deep;
-    // const parrentDeep = event.target.parentElement.dataset.deep;
+    const parrentDeep = event.target.parentElement.dataset.deep;
     const numberOfItem: number = event.target.dataset.index;
-    // console.log(itemDeep, this.deep);
-    if(itemClassName === 'item') {
+    if (parrentDeep < this.deep) {
+      this.deep += parrentDeep;
+    }
+    if (itemClassName === 'item') {
       this.itemPath.push(numberOfItem);
       this.createNewItemList(currentItem);
       currentItem.className = 'active-item';
-    } else if( itemClassName === 'active-item' ) {
+    } else if (itemClassName === 'active-item') {
       currentItem.className = 'item';
       currentItem.innerHTML = 'Item' + numberOfItem;
-      this.itemPath.splice(itemDeep, this.deep);
+      this.itemPath.splice(parrentDeep, this.itemPath.length);
     }
   }
 
-
   createNewItemList(currentItem) {
-    // this.deep++;
-    for(let i = 1; i < this.itemList.length+1; i++) {
+    this.deep++;
+    const currentDeep = this.deep.toString();
+    for (let i = 1; i < this.itemList.length + 1; i++) {
+      const index = i.toString();
       const newItem = document.createElement('div');
-      newItem.innerHTML = 'Item' + i;
+      newItem.innerHTML = 'Item' + index;
       newItem.style.marginLeft = '10px';
-      newItem.setAttribute('data-index', i);
-      // newItem.setAttribute('data-deep', this.deep);
+      newItem.setAttribute('data-index', index);
+      newItem.setAttribute('data-deep', currentDeep);
       newItem.classList.add('item');
       currentItem.appendChild(newItem);
     }
   }
 
+  filterItems() {
+    const input = document.querySelector('#filterInput') as HTMLInputElement;
+    const filterData = input.value.toUpperCase();
+    const itemFromList = document.querySelectorAll('.item');
+    const itemArray = Array.from(itemFromList);
+    itemArray.forEach((item: HTMLElement) => {
+      if (item.innerHTML.toUpperCase().indexOf(filterData) > -1) {
+        item.style.display = '';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
 }
